70450cfc874af949d81cac3c6d1888c59d93035f
Jose <jdtomanan@up.edu.ph>
2024-07-22 20:49:27 +0800

    feat: file change (im not sure if this is tama)

--- src/app.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	let src = 'static/test.jpg';
-	let title = 'You\'re on your own, kid';
-</script>
-
-<img {src} alt="{title} lyrics" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	let src = 'static/test.jpg';
+	let title = 'You\'re on your own, kid';
+</script>
+
+<img {src} alt="{title} lyrics" />
