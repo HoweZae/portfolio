1aa529f4baf2c4061b6e494247ee5bd4dd3841e3
Jose <jdtomanan@up.edu.ph>
2024-07-26 09:29:44 +0800

    feat: started barebones

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../app.css";
+    import "../global.css";
+</script>
+
+<h1 class="text-3xl font-bold">Heya! I'm Jose.</h1>
+<h3 class="text-l">
+    I am a 3rd year BS Computer Science Student in UP Diliman.
+    Thanks for visiting my portfolio!
+</h3>
+
+<h2 class="text-xl">What have I been up to?</h2>
+<h3>
+    Tackle Head
+</h3>
