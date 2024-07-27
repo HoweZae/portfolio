4362e0774e2338c44cdb4d552d839684976d18b9
Jose <jdtomanan@up.edu.ph>
2024-07-27 21:19:54 +0800

    feat: added divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,41 @@
     import "../global.css";
 </script>
 
-<div class="mx-40 my-20">
-    <h1 class="text-3xl my-2">
+<header class="bg-slate-800 p-5 rounded-xl">
+    <h1 class="text-3xl my-2 font-bold italic underline">
         Hey ya! I'm Jose.
     </h1>
     <p class="text-l text-slate-400">
-        I am a 3rd year BS Computer Science student at the University of the Philippines Diliman.
-        <br/>
-        Please feel free to visit my <a class="underline text-slate-300" href="https://www.linkedin.com/in/jedtomanan/">LinkedIn profile</a> here.
-        <br/>
-        Pleasure to meet you!
+        I am a 3rd year BS Computer Science student from the University of the Philippines Diliman.
+        It's a pleasure to meet you!
     </p>
 
     <!-- Placeholder -->
-    <img class="content-center bg-slate-600 mx-3 my-6 p-3"
+    <img class="content-center bg-slate-600 hover:bg-slate-500 mx-10 my-6 p-3"
         src="/favicon.png" alt="Logo"/>
+</header>
 
-    <h2 class="text-xl my-2">
-        What have I been up to?
-    </h2>
-    <h3 class="text-l text-slate-400">
-        A bunch of stuff, honestly. But here are the highlights:
+<div class="bg-slate-800 my-5 p-5 rounded-xl">
+    <h2 class="text-xl">What roles have I done?</h2>
+    <h3>
+        You can see it in full detail in my <a class="underline text-slate-300" href="https://www.linkedin.com/in/jedtomanan/">LinkedIn profile</a> here.
+        Here are the highlights:
     </h3>
+    <div>
 
+    </div>
+
+    <h2 class="text-xl mt-5">What projects have I made?</h2>
+    <h3>Check them out here!</h3>
+    <div>
+
+    </div>
+
+    <h2 class="text-xl mt-5">What do I do in my free time?</h2>
+    <h3>Also check them out here!</h3>
+
+</div>
+
+<div class="bg-slate-800 my-5 p-5 rounded-xl">
+    <h2 class="text-xl">Where can you find me?</h2>
 </div>
