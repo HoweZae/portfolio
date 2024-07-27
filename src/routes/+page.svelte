051faa3c1de002c5814b0b484dc0b435b79f443e
Jose <jdtomanan@up.edu.ph>
2024-07-27 20:24:20 +0800

    feat: improved padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,26 @@
     import "../global.css";
 </script>
 
-<div class="mx-32 my-32">
-    <h1 class="text-3xl leading-8">Heya! I'm Jose.</h1>
-    <h3 class="text-l text-slate-400 leading-8">
+<div class="mx-40 my-20">
+    <h1 class="text-3xl my-2">
+        Hey ya! I'm Jose.
+    </h1>
+    <p class="text-l text-slate-400">
         I am a 3rd year BS Computer Science student at the University of the Philippines Diliman.
-        You can also visit my
-        <a class="underline text-slate-300" href="https://www.linkedin.com/in/jedtomanan/">LinkedIn profile</a>.
-        Nice to meet you!
-    </h3>
+        <br/>
+        Please feel free to visit my <a class="underline text-slate-300" href="https://www.linkedin.com/in/jedtomanan/">LinkedIn profile</a> here.
+        <br/>
+        Pleasure to meet you!
+    </p>
 
-    <div class="self-center">
-        <!-- Placeholder -->
-        <img src="/favicon.png" alt="Logo"/>
-    </div>
+    <!-- Placeholder -->
+    <img class="content-center bg-slate-600 mx-3 my-6 p-3"
+        src="/favicon.png" alt="Logo"/>
 
-    <h2 class="text-xl leading-8">What have I been up to?</h2>
-    <h3 class="text-l text-slate-400 leading-8">
+    <h2 class="text-xl my-2">
+        What have I been up to?
+    </h2>
+    <h3 class="text-l text-slate-400">
         A bunch of stuff, honestly. But here are the highlights:
     </h3>
 
