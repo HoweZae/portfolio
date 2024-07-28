f55aa490a7611fbc475213de4a4c2527c42d7ec3
Jose <jdtomanan@up.edu.ph>
2024-07-28 14:37:22 +0800

    feat: shifted to light mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,11 +17,11 @@
     ]
 </script>
 
-<header class="bg-slate-800 p-5 rounded-xl">
+<header class="bg-gradient-to-br from-white to-slate-200 to-90% p-5 rounded-xl shadow-lg">
     <h1 class="text-3xl my-2 font-bold italic underline">
         Hey ya! I'm Jose.
     </h1>
-    <p class="text-l text-slate-400">
+    <p class="text-l">
         I am a 3rd year BS Computer Science student from the University of the Philippines Diliman.
         Thanks for visiting this page, it's nice to meet you!
         <br>
@@ -39,24 +39,24 @@
 </header>
 
 <div class="flex my-5">
-    <div class="basis-1/2 bg-slate-800 mr-3 p-5 rounded-xl">
+    <div class="basis-1/2 bg-white mr-3 p-5 rounded-xl shadow-lg">
         <h2 class="text-xl">What have I been up to?</h2>
         <h3>
-            See these in full detail in <a class="underline text-slate-300" href={link.li}>LinkedIn</a>.
+            See these in full detail in <a class="underline text-slate-600" href={link.li}>LinkedIn</a>.
             Here are my highlights:
         </h3>
     </div>
 
-    <div class="basis-1/2 bg-slate-800 ml-3 p-5 rounded-xl">
+    <div class="basis-1/2 bg-white ml-3 p-5 rounded-xl shadow-lg">
         <h2 class="text-xl">What projects have I made?</h2>
         <h3>
-            See these in full detail in <a class="underline text-slate-300" href={link.gh}>GitHub</a>.
+            See these in full detail in <a class="underline text-slate-600" href={link.gh}>GitHub</a>.
             Here are my highlights:
         </h3>
     </div>
 </div>
 
-<div class="bg-slate-800 my-5 p-5 rounded-xl">
+<div class="bg-white my-5 p-5 rounded-xl shadow-lg">
     <h2 class="text-xl">What do I do in my free time?</h2>
     <h3 class="text-justify">
         Outside of computer science, I also like to hit the gym, play competitive video games, and play basketball.
@@ -64,12 +64,12 @@
     </h3>
 </div>
 
-<div class="bg-slate-800 my-5 p-5 rounded-xl">
+<div class="bg-white my-5 p-5 rounded-xl shadow-lg">
     <h2 class="text-xl">Where can you find me?</h2>
     <h3>You're not getting it you fucking cow</h3>
 </div>
 
-<div class="bg-slate-800 my-5 p-5 rounded-xl">
+<div class="bg-white my-5 p-5 rounded-xl shadow-lg">
     <h3 class="text-white-900 text-justify">
         This page is written with Svelte and Tailwind.
         If you haven't noticed, I tried to fill out this web page with as much of my knowledge as possible.
