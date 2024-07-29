9a1bd988517a5af3b3af690ddc83511d5e4f9b88
Jose <jdtomanan@up.edu.ph>
2024-07-29 19:57:54 +0800

    feat: added me in header + structure changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,28 +19,36 @@
     ]
 </script>
 
-<header class="bg-white px-7 py-5 rounded-xl shadow-lg">
-    <h1 class="text-3xl mb-2 font-bold italic">
-        Hey, ya! I'm Jose Tomanan.
-    </h1>
-    <h4 class="text-lg">
-        I am a 3rd year BS Computer Science student from the University of the Philippines Diliman.
-        Nice to meet you!
-        <!-- You can visit my socials below: -->
-    </h4>
-
-    <div class="flex mt-3">
-    {#each header as image}
-        <a href={image.link}
-            class="basis-1/4 flex justify-center
-                bg-gradient-to-r from-slate-100 to-slate-100 to-75%
-                hover:from-slate-100 active:from-slate-100
-                {image.color}
-                mx-2 p-2 rounded-xl"
-        ><img class="h-8" src={image.logo} alt="Logo" /></a>
-    {/each}
-    </div>
-</header>
+<section class="mb-0">
+    <header class="flex">
+        <img 
+            class="flex justify-center content-center cmy-2 h-56 border-double border-2 border-orange-300"
+            src="/me.JPG"
+            alt="Me"
+        />
+        <div class="grow ml-3">
+            <h1 class="text-3xl ml-1 mt-3 mb-2 font-bold italic">
+                Hey, ya! I'm Jose Tomanan.
+            </h1>
+            <h4 class="text-lg ml-2">
+                I am a BS Computer Science junior student from the University of the Philippines Diliman.
+                <br> Thanks for visiting this page, it's nice to meet you!
+                <br> Feel free to get to know me better through the following platforms:
+            </h4>
+            <div class="flex mt-3">
+                {#each header as image}
+                    <a href={image.link}
+                        class="basis-1/4 flex justify-center
+                            bg-gradient-to-r from-slate-100 to-slate-100 to-75%
+                            hover:from-slate-100 active:from-slate-100
+                            {image.color}
+                            mx-2 p-2 rounded-xl"
+                    ><img class="h-8" src={image.logo} alt="Logo" /></a>
+                {/each}
+                </div>
+        </div>
+    </header>
+</section>
 
 <div class="flex">
     <section class="basis-1/2 mr-3 mb-0">
@@ -59,7 +67,7 @@
 
         <!-- <article class="hover:bg-slate-200"> -->
         <article>
-            <h3 class="text-lg">Head Giver</h3>
+            <h3 class="text-lg">Director for Engineering Committee</h3>
             <h4 class="italic">UP Center for Student Innovations (UP CSI)</h4>
             <h5 class="text-slate-500 text-sm mt-1">September 2023 - November 2023</h5>
         </article>
@@ -121,7 +129,7 @@
             jdtomanan@up.edu.ph
         </h5>
         <h5 class="basis-1/3 flex justify-center bg-gradient-to-r hover:from-slate-100 hover:to-slate-100 p-2 rounded-xl">
-            jdtomanan@up.edu.ph
+            +63 947 301 3664
         </h5>
         <h5 class="basis-1/3 flex justify-center bg-gradient-to-r hover:from-slate-100 hover:to-slate-100 p-2 rounded-xl ml-2">
             jdtomanan@up.edu.ph
