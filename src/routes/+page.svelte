4c333a17d2f7ff381bfb026e9a14c82e02e81391
Jose <jdtomanan@up.edu.ph>
2024-07-30 22:11:02 +0800

    feat: moved functions, constants from +page.svelte to index.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,25 +4,11 @@
 
     import Article from "./Article.svelte";
 
-    const link = {
-        li: "https://www.linkedin.com/in/jedtomanan/",
-        gh: "https://github.com/HoweZae",
-        fb: "https://www.facebook.com/jedtomanan/",
-        ig: "https://www.instagram.com/jose.tmnn/",
-        twt: "https://x.com/howe_zae",
-    }
+    import * as helpers from "$lib/index.js";
 
-    const header = [
-        {logo: "/logo/li.png", color: "hover:to-cyan-200 active:from-cyan-300 active:to-cyan-300", link: link.li, handle: "jedtomanan"},
-        {logo: "/logo/gh.svg", color: "hover:to-purple-200 active:from-purple-300 active:to-purple-300", link: link.gh, handle: "HoweZae"},
-        {logo: "/logo/fb.png", color: "hover:to-blue-200 active:from-blue-300 active:to-blue-300", link: link.fb, handle: "jedtomanan"},
-        {logo: "/logo/ig.png", color: "hover:to-red-100 active:from-red-200 active:to-red-200", link: link.ig, handle: "jose.tmnn"},
-        {logo: "/logo/twt.png", color: "hover:to-sky-100 active:from-sky-200 active:to-sky-200", link: link.twt, handle: "howe_zae"},
-    ]
-
-    function linkClicked() {
-        console.log("Link clicked. Hi mom!");
-    }
+    let link = helpers.link;
+    let header = helpers.header;
+    let linkClicked = helpers.linkClicked;
 </script>
 
 <header class="flex">
@@ -48,6 +34,7 @@
                     hover:from-slate-100
                     {image.color}
                     mx-2 p-2 rounded-xl"
+                on:click={ linkClicked }
             ><img class="h-8" src={image.logo} alt="Logo" /></a>
         {/each}
         </div>
