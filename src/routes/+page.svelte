313db596b5b4b4ecd3b5d03869d1b7e8c9992033
Jose <jdtomanan@up.edu.ph>
2024-08-11 22:05:54 +0800

    fix: moved intro to main page (and changed <header> to <div>)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Article from "../lib/components/Article.svelte";
     import ClickToCopy from "../lib/components/ClickToCopy.svelte";
 
-    import { link, linkClicked } from "$lib";
+    import { link, header, buttonHover, linkClicked } from "$lib";
 
     // import { tweened, spring } from "svelte/motion";
     // import { elasticOut } from "svelte/easing";
@@ -12,6 +12,38 @@
     //     easing: elasticOut
     // });
 </script>
+
+<div class="flex">
+    <div class="h-52 w-52 rounded-lg border-double border-4 border-sky-600 mr-2 p-1">
+        <img 
+            class="rounded-sm"
+            src="/images/me.JPG"
+            alt="Me"
+        />
+    </div>
+    <section class="flex flex-col justify-between grow my-0 p-6">
+        <h1 class="text-3xl font-bold italic">
+            Heya! I'm Jose Tomanan.
+        </h1>
+        <h4 class="text-lg">
+            I am a 3rd year BS Computer Science student from University of the Philippines Diliman.
+            <br>
+            Thanks for visiting! Get to know me more:
+        </h4>
+        <div class="flex">
+            {#each header as image}
+            <a 
+                href={ image.link }
+                class="basis-1/5 flex justify-center bg-gradient-to-r from-white to-white to-75% {image.color} p-2 rounded-xl"
+                target="_blank"
+                on:focus={ buttonHover }
+            >
+                <img class="h-8" src={image.logo} alt="Logo" />
+            </a>
+            {/each}
+        </div>
+    </section>
+</div>
 
 <section>
     <h2>What do I do?</h2>
