df30adaf8602657f0b4f26b2d46e07dc0177a3ea
Jose <jdtomanan@up.edu.ph>
2024-08-16 16:35:16 +0800

    feat: created navbar centered navbar and main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,24 @@
     import "../global.css";
 </script>
 
-<header>
-    
-</header>
+<nav class="h-16 flex flex-col justify-center bg-white border-b-2 border-sky-500" >
+    <navbar class="max-w-screen flex justify-center gap-8 font-['Avenir']" >
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a>Top</a>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a>Projects</a>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a>Roles</a>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <a>Contact</a>
+    </navbar>
+</nav>
 
-<main>
-    <slot></slot>
-</main>
+<premain class="flex justify-center">
+    <main class="mx-6 min-h-screen max-w-screen-lg flex flex-col">
+        <slot></slot>
+    </main>
+</premain>
 
 <footer>
     <h6 class="text-center text-slate-400">
