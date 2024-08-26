09ef317b10680e3781d5a780f4a786f1ac63376c
Jose <jdtomanan@up.edu.ph>
2024-08-26 10:27:02 +0800

    feat: links (kulang pa isa)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -107,12 +107,13 @@
         <p>
             When not working, however, I prefer to spend my time away from the keyboard than in front of it.
             I like to stay active and explore different things, but my favorite ways to spend my free time are with
-            <a href="_blank" class="underline underline-offset-2 hover:text-sky-500">weightlifting</a> and
+            <a href="https://www.instagram.com/stories/highlights/17992555250680811/" class="underline underline-offset-2 hover:text-sky-500">weightlifting</a> and
             <a href="_blank" class="underline underline-offset-2 hover:text-sky-500">basketball</a>.
         </p>
 
         <p>
-            I am also an <a href="_blank" class="underline underline-offset-2 hover:text-sky-500">avid Swiftie</a>: 
+            I am also an 
+            <a href="https://www.instagram.com/p/B6p4rR_hoUC/" class="underline underline-offset-2 hover:text-sky-500">avid Swiftie</a>: 
             I'd bet my left leg I know more about Taylor Swift than you!
         </p>
     </section>
