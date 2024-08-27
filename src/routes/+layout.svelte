eb9a80e16cf388394ffe796c7c8bd8b254a433c9
Jose <jdtomanan@up.edu.ph>
2024-08-27 11:22:30 +0800

    feat: more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.css";
 </script>
 
-<div class="app bg-fixed bg-slate-200">
+<div class="app bg-fixed bg-slate-200 scroll-smooth">
     <nav 
         class="p-4 bg-white border-b-2 border-sky-500 max-w-screen flex items-center justify-center gap-8 font-['Avenir'] fixed w-full bg-opacity-80 z-50"
     >
