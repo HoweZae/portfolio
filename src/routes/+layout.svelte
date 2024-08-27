6598272ff3d7233cf0a1693e011c402c917026cd
Jose <jdtomanan@up.edu.ph>
2024-08-27 21:24:14 +0800

    feat: move scroll-smooth to <html>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import "../app.css";
 </script>
 
-<div class="app bg-fixed scroll-smooth">
+<div class="app bg-fixed">
     <nav 
         class="p-4 bg-white border-b-2 border-sky-500 max-w-screen flex items-center justify-center gap-8 font-['Avenir'] fixed w-full bg-opacity-80 z-50"
     >
