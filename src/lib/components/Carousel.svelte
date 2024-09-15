0840df7a59ba6632a7b8c0bf6c3773fcd4abce46
HoweZae <jdtomanan@up.edu.ph>
2024-09-15 16:49:39 +0800

    feat: Replaced Carousel with static

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,39 +1,38 @@
-<!--
 <enhanced:img 
-    class="h-[25rem] w-full object-cover"
-    src="$lib/images/header.jpg"
+    class="object-cover overflow-clip h-[25rem] w-full"
+    src="$lib/images/hero.jpg"
     alt="Me"
 />
--->
 
 <!-- SOURCE: https://flowbite.com/docs/components/carousel/ -->
-<div id="indicators-carousel" class="relative w-full" data-carousel="slide">
+
+<!-- <div id="indicators-carousel" class="relative w-full" data-carousel="slide"> -->
     <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-t-lg md:h-96">
-         <!-- Item 1 -->
+    <!-- <div class="relative h-56 overflow-hidden rounded-t-lg md:h-96">
         <div class="duration-700 ease-in-out" data-carousel-item="active">
             <enhanced:img src="$lib/images/carousel/header.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
 
-        <!-- Item 2 -->
         <div class="duration-700 ease-in-out" data-carousel-item>
             <enhanced:img src="$lib/images/carousel/header.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
 
-        <!-- Item 3 -->
         <div class="duration-700 ease-in-out" data-carousel-item>
             <enhanced:img src="$lib/images/carousel/header.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
-    </div>
+    </div> -->
 
     <!-- Slider indicators -->
+    <!--
     <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
         <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
     </div>
+    -->
 
     <!-- Slider controls -->
+    <!-- 
     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
@@ -42,7 +41,9 @@
             <span class="sr-only">Previous</span>
         </span>
     </button>
+    -->
     
+    <!-- 
     <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
@@ -51,4 +52,5 @@
             <span class="sr-only">Next</span>
         </span>
     </button>
-</div>
+    -->
+<!-- </div> -->
