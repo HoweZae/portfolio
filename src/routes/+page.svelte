c1dd8e3a41cda391318db0b3fb5868a3f5ae7479
JoseTomanan <jdtomanan@up.edu.ph>
2026-09-15 23:02:34 +0800

    style(frontend): rename hoverable-link utility to hoverable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,7 +105,7 @@
         </span>,
         currently working as a backend developer at METR.
         I use AI to develop my passion projects, one of which is a
-        <a href={link.gh_hooper_game} class="hoverable-link font-medium" target="_blank" rel="noopener noreferrer">
+        <a href={link.gh_hooper_game} class="hoverable font-medium" target="_blank" rel="noopener noreferrer">
           duel-style basketball game
         </a>
         in Godot. Prototype coming soon!
